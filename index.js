b3fc9e97275a28268bc643e9bdfe186747ef27bc
singhsunpreet98s <singhsunpreet98s@gmail.com>
2022-08-21 16:26:44 +0530

    age feature in object

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const myName = {
    firstName: "sunpreet",
-   lastName: "singh"
+   lastName: "singh",
+   age: 12
 };
 // adding changes in main branch
 
 // here i am adding a new feature of age
-const age = 10;
+
 console.log(myName.firstName, myName.lastName);
